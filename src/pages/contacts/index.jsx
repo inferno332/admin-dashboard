dce8d187223600d82b455057712a4ff920879b34
Cong Khoa Huynh <bhuynhcongkhoa@gmail.com>
2022-11-02 23:04:54 +0700

    update DataTable component

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -1,44 +1,38 @@
-import { Box, useTheme } from '@mui/material';
-import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-import { tokens } from '../../theme';
+import { useState } from 'react';
+import { Box } from '@mui/material';
+import { GridToolbar } from '@mui/x-data-grid';
 import { mockDataContacts } from '../../data/mockData';
 
+import DataTable from '../../components/DataTable';
 import Header from '../../components/Header';
 
 function Contacts() {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
+    const [pageSize, setPageSize] = useState(10);
 
     const columns = [
         { field: 'id', headerName: 'ID', flex: 0.5 },
-        { field: 'registraId', headerName: 'REGISTRA ID' },
+        { field: 'registrarId', headerName: 'REGISTRAR ID' },
         { field: 'name', headerName: 'NAME', flex: 1, cellClassName: 'name-column--cell' },
         { field: 'age', headerName: 'AGE', type: 'number', headerAlign: 'left', align: 'left' },
         { field: 'phone', headerName: 'PHONE NUMBER', flex: 1 },
         { field: 'email', headerName: 'EMAIL', flex: 1 },
         { field: 'address', headerName: 'ADDRESS', flex: 1 },
         { field: 'city', headerName: 'CITY', flex: 1 },
-        { field: 'zipcode', headerName: 'ZIPCODE', flex: 1 },
+        { field: 'zipCode', headerName: 'ZIPCODE', flex: 1 },
     ];
 
     return (
         <Box m="20px">
             <Header title="CONTACTS" subtitle="List of contacts" />
-            <Box
-                m="40px 0 0 0"
-                height="75vh"
-                sx={{
-                    '& .MuiDataGrid-root': { border: 'none' },
-                    '& .MuiDataGrid-cell': { borderBottom: 'none' },
-                    '& .name-column--cell': { color: colors.greenAccent[300] },
-                    '& .MuiDataGrid-columnHeaders': { backgroundColor: colors.blueAccent[700], borderBottom: 'none' },
-                    '& .MuiDataGrid-virtualScroller': { backgroundColor: colors.primary[400] },
-                    '& .MuiDataGrid-footerContainer': { backgroundColor: colors.blueAccent[700], borderTop: 'none' },
-                    '& .MuiDataGrid-toolbarContainer .MuiButton-text': { color: `${colors.grey[100]} !important` },
-                }}
-            >
-                <DataGrid rows={mockDataContacts} columns={columns} components={{ Toolbar: GridToolbar }} />
-            </Box>
+            <DataTable
+                rows={mockDataContacts}
+                columns={columns}
+                styling
+                components={{ Toolbar: GridToolbar }}
+                pageSize={pageSize}
+                onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                rowsPerPageOptions={[5, 10, 15, 20]}
+            />
         </Box>
     );
 }
